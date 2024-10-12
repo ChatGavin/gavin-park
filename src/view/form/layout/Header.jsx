aa626cc2f4a2116904666db7686c6ce64e02e654
0xduer <0xduer@gmail.com>
2024-10-12 22:18:17 +0800

    feat:modify header style and layout and add icon action

--- src/view/form/layout/Header.jsx
+++ src/view/form/layout/Header.jsx
@@ -1,10 +1,29 @@
 import styles from './layout.module.css'
+import {Tooltip} from 'antd'
+import {AntDesignOutlined, GithubOutlined} from '@ant-design/icons'
 
 export default function App() {
     return (
         <div className={styles.header}>
-            <div className={styles.title}>
-                ProFormFields
+            <div className={styles.flex}>
+                <div className={styles.title}>ProFormFields</div>
+                <div>
+                    <Tooltip title="Ant Design">
+                        <AntDesignOutlined
+                            className={styles.icon}
+                            style={{marginRight: '5px', color: '#1890ff'}}
+                            onClick={() => {
+                                window.open('https://ant.design/components/overview-cn')
+                            }}/>
+                    </Tooltip>
+                    <Tooltip title="Github">
+                        <GithubOutlined
+                            className={styles.icon}
+                            onClick={() => {
+                                window.open('https://github.com/linidea/antd-visual')
+                            }}/>
+                    </Tooltip>
+                </div>
             </div>
         </div>
     );
