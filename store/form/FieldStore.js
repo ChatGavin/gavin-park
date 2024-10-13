cbe4619dd77c85b879a2a836bfc39dd8e6f2d5b3
0xduer <0xduer@gmail.com>
2024-10-14 02:56:41 +0800

    feat:set Fields observable add new function getActiveComponent and add import component

--- store/form/FieldStore.js
+++ store/form/FieldStore.js
@@ -1,22 +1,30 @@
-import {makeAutoObservable} from "mobx";
+import {makeAutoObservable, observable} from "mobx";
 import Fields from "./Fields.js";
 
 class FieldStore {
 
     constructor() {
+        // 确保 Fields 是响应式的
+        this.Fields = observable(Fields);
         makeAutoObservable(this);
     }
 
     // 获取所有字段
     getFields() {
-        return Fields;
+        return this.Fields;
     }
 
     // 设置激活状态
     setActive(name) {
-        Fields.forEach(item => {
+        if (!name) return;
+        this.Fields.forEach(item => {
             item.active = item.name === name;
         });
+    }
+
+    // 获取激活的组件
+    getActiveComponent() {
+        return this.Fields.find(item => item.active).component;
     }
 }
 
--- store/form/Fields.js
+++ store/form/Fields.js
@@ -1,13 +1,17 @@
+import ProFormText from "@view/form/fields/ProFormText.jsx";
+import ProFormDigit from "@view/form/fields/ProFormDigit.jsx";
 // 表单字段
 export default [
     {
         active: true,
         name: 'ProFormText',
         desc: '用于输入各类文本',
+        component: ProFormText
     },
     {
         active: false,
         name: 'ProFormDigit',
         desc: '用于输入数字，它自带了一个格式化',
+        component: ProFormDigit
     }
 ]
