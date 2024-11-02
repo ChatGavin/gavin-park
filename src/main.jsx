c974dd1411f0003228a818914b5f6e90f1c98120
0xduer <0xduer@gmail.com>
2024-11-02 14:44:45 +0800

    feat:add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
+//
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//
+import {BrowserRouter} from "react-router-dom";
+// css reset
 import 'normalize.css'
 import '@style/index.js'
-//
+// main entry
 import App from '@view/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <App/>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
 )
--- src/view/App.jsx
+++ src/view/App.jsx
@@ -1,9 +1,16 @@
-// 入口
-import MyForm from '@view/antd/form/index.jsx'
+//
+import {Routes, Route} from "react-router-dom";
+// page entry
+import ProForm from '@view/antd/form/index.jsx'
+import Amis from '@view/amis/index.jsx'
 
 function App() {
     return (
-        <MyForm/>
+        <Routes>
+            <Route path="/" element={<ProForm/>}/>
+            <Route path="/amis" element={<Amis/>}/>
+            <Route path="/form" element={<ProForm/>}/>
+        </Routes>
     )
 }
 
