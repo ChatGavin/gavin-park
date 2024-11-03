152b4671728b2bad276bd03ae03d56a9a5175e88
0xduer <0xduer@gmail.com>
2024-11-03 23:04:07 +0800

    feat:add page 404

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -1,15 +1,17 @@
 //
 import {Routes, Route} from "react-router-dom";
 // page entry
-import ProForm from '@view/antd/form/index.jsx'
-import Amis from '@view/amis/index.jsx'
+import AmisEditor from '@view/amis/index.jsx'
+import AntdForm from '@view/antd/form/index.jsx'
+import NotFound from '@view/error/NotFound.jsx'
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<ProForm/>}/>
-            <Route path="/amis" element={<Amis/>}/>
-            <Route path="/form" element={<ProForm/>}/>
+            <Route path="/" element={<AmisEditor/>}/>
+            <Route path="/amis-editor" element={<AmisEditor/>}/>
+            <Route path="/antd-form" element={<AntdForm/>}/>
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
     )
 }
---
+++ src/view/error/NotFound.jsx
@@ -0,0 +1,18 @@
+//
+import {Result, Button} from "antd";
+//
+import {useNavigate} from "react-router-dom";
+
+function App() {
+    const navigate = useNavigate()
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
+        />
+    )
+}
+
+export default App
