d625bde424a108f4503fd659e2a7dcae49058c13
0xduer <0xduer@gmail.com>
2024-11-12 22:42:34 +0800

    feat:rename form router

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -2,14 +2,14 @@
 import {Routes, Route} from "react-router-dom";
 // page entry
 import Home from '@view/home/index.jsx'
-import AntdForm from '@view/form/index.jsx'
+import Form from '@view/form/index.jsx'
 import NotFound from '@view/error/NotFound.jsx'
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/antd-form" element={<AntdForm/>}/>
+            <Route path="/form" element={<Form/>}/>
             <Route path="*" element={<NotFound/>}/>
         </Routes>
     )
