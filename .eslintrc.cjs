0772432e5ad7196177407f7dfc0c96a377d1c159
0xduer <0xduer@gmail.com>
2024-11-14 01:22:56 +0800

    feat:modify project config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
-    settings: {react: {version: '18.2'}},
+    settings: {react: {version: '18.3'}},
     plugins: ['react-refresh'],
     rules: {
         'react/jsx-no-target-blank': 'off',
