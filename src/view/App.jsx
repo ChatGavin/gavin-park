7e9bdb9439f249ae769162d9bc6ff31b27ff6742
0xduer <0xduer@gmail.com>
2024-11-17 01:00:15 +0800

    feat:add main section style

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -10,7 +10,7 @@ const WithLayout = () => {
     return (
         <div className='container'>
             <Header/>
-            <Outlet/>
+            <Outlet className='main'/>
         </div>
     )
 }
