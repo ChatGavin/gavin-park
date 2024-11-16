30c89bfb17d0c9fe102bebca54dc125e4324ab24
0xduer <0xduer@gmail.com>
2024-11-16 14:15:50 +0800

    feat:del component form-header

--- src/view/form/index.jsx
+++ src/view/form/index.jsx
@@ -1,7 +1,6 @@
 //
 import styles from './style/layout.module.css'
 //
-import Header from './layout/Header.jsx'
 import Left from './layout/Left.jsx'
 import Middle from './layout/Middle.jsx'
 import Right from './layout/Right.jsx'
@@ -9,7 +8,6 @@ import Right from './layout/Right.jsx'
 function App() {
     return (
         <div className={styles.container}>
-            <Header/>
             <div className={styles.main}>
                 <Left/>
                 <Middle/>
