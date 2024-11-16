eb8cf48cc4b72295282284e31a8d94a283b7a112
0xduer <0xduer@gmail.com>
2024-11-16 14:38:30 +0800

    feat:use Outlet to render children routes

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -1,16 +1,40 @@
 //
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Outlet} from "react-router-dom";
 // pages routes
+import Header from '@component/layout/Header.jsx'
 import Home from '@view/home/index.jsx'
 import Form from '@view/form/index.jsx'
 import NotFound from '@view/error/NotFound.jsx'
+// has common layout
+const WithLayout = () => {
+    return (
+        <div className='container'>
+            <Header/>
+            <Outlet/>
+        </div>
+    )
+}
+// without layout
+const WithoutLayout = () => {
+    return (
+        <div className='container'>
+            <Outlet/>
+        </div>
+    )
+}
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/form" element={<Form/>}/>
-            <Route path="*" element={<NotFound/>}/>
+            {/*use Outlet to render children routes*/}
+            <Route element={<WithLayout/>}>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/form" element={<Form/>}/>
+            </Route>
+            {/*custom pages*/}
+            <Route element={<WithoutLayout/>}>
+                <Route path="*" element={<NotFound/>}/>
+            </Route>
         </Routes>
     )
 }
