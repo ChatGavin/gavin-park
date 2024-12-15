71e9416dbf421e19e88488c8b4a2fd6b3a3333ef
gavin <0xduer@gmail.com>
2024-12-16 00:12:49 +0800

    init:add request to local llama3

---
+++ packages/llama-3-8b/README.md
@@ -0,0 +1,5 @@
+# Llama-3.1-8B-Instruct
+
+## 快速开始
+
+ollama（llama3） -- python（request ollama） -- python（save db）-- python（response fe）-- antdx（render）
--- packages/llama-3-8b/main.py
+++ packages/llama-3-8b/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+# from huggingface_hub import login
+#
+# login(token="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")
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
+response = requests.get("http://127.0.0.1:11434")
+print(response.text)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import requests
+import json
 
+# 定义 URL 和请求数据
+url = "http://localhost:11434/api/generate"
+data = {
+    "model": "llama3",
+    "prompt": "介绍一下 vue 和 react 的不同",
+    "stream": False
+}
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 发起 POST 请求
+response = requests.post(url, json=data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 打印返回结果
+if response.status_code == 200:
+    print("成功响应:")
+    print(response.json())  # 打印 JSON 格式的返回数据
+else:
+    print(f"请求失败: {response.status_code}")
+    print(response.text)  # 打印错误信息
